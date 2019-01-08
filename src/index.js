c334715db0723f5d27ca4244000feb946015cc91
Crystal C <crystal.contr@gmail.com>
2019-01-08 05:42:35 -0600

    Added Spinner ui/js to display while page loads. Put the if-else statements that will display content in its own function and returned that function in our render in order to avoid if-else statements directly inside the render.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: ''};
@@ -12,16 +13,25 @@ class App extends React.Component {
         );
     }
 
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+          return <SeasonDisplay lat={this.state.lat} />
+        }
+        if (!this.state.errorMessage && !this.state.lat) {
+          return <Spinner message="Please accept location request." />
+        }
+    }
+
     render() {
-      if (this.state.errorMessage && !this.state.lat) {
-          return <div>Error: {this.state.errorMessage}</div>
-      }
-      if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat} />
-      }
-      if (!this.state.errorMessage && !this.state.lat) {
-        return <div>Loading!</div>
-      }
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+    
     }
 }
 
